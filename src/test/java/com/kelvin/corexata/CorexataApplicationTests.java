06c9dcd506b5534ebd381d259842eafad01603c7
Kelvin Souza Gonçalves <kelvinsouzag@gmail.com>
2025-07-02 21:12:32 -0300

    inicio

---
+++ src/test/java/com/kelvin/corexata/CorexataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelvin.corexata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorexataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
